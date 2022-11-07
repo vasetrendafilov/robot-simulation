b35cad93252d29f69c6f2e9ba089b0e48f6f41f9
Vasil Trendafilov <vase_trendafilov@hotmail.com>
2022-11-07 01:05:36 +0100

    import existing robot arm
    - importing dh param from first lines in file
    - adding the constraints from the import
    - class for the simulation to join multiple arms

--- packages/serial_link_robot.py
+++ packages/serial_link_robot.py
@@ -6,58 +6,101 @@ import os
 import pybullet as p
 import time 
 
+class PybulletSimulation:
+
+    def __init__(self, connection_mode = p.GUI, fps = 60, gravity = (0, -9.8, 0)):
+        self.connection_mode = connection_mode
+        self.time_step= 1/fps
+        self.gravity = gravity
+
+    def configure(self):
+        p.setTimeStep(self.time_step)
+        p.setGravity(self.gravity[0],self.gravity[1],self.gravity[2])
+        p.setAdditionalSearchPath(os.getcwd())
+        
+        p.configureDebugVisualizer(lightPosition= (0,0,10))
+        p.resetDebugVisualizerCamera( cameraDistance=15, cameraYaw=15, cameraPitch=-20, cameraTargetPosition=[0,0,0])
+        #p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS,0)
+
+    def connect(self):
+        if p.connect(self.connection_mode) != -1: # connected
+            self.configure()
+            return True
+        return False
+
 class SerialLinkRobot:
     """
     A class to easily create and interact with robotic arm.
     """
     
-    def __init__(self, offset = (0,0,0), orientation = (0,0,0,1)):
+    def __init__(self, offset = (0,0,0), orientation = (0,0,0,1), name = 'my_robot'):
+        self.name = name
         self.robot = None
         self.offset = offset
         self.orientation = orientation
-        self.connect()
-        self.reset()
-       
-
-    
-    def reset(self):
-        """ Reset the robotic arm data. """
+        #sympy vars
         self.links = []
         self.constraints = []
         self.joint_variables = []
         self.subs_joints = []
         self.subs_additional = []
+        #pybullet vars
         self.joint_sliders= []
         self.joint_ids = []
         self.joint_axes = []
 
-    def connect(self):
-        p.connect(p.GUI)
-        self.configuration()
-        
+        if not p.isConnected():
+            sim = PybulletSimulation()
+            if sim.connect():
+                self.time_step = sim.time_step 
 
-    def interact(self):
-
-        subs = self.subs_joints + self.subs_additional
-        DH_params = sp.Matrix(self.links).subs(subs).evalf()
-        dh2urdf = DH2Urdf(DH_params.tolist(),self.constraints)
-        dh2urdf.save_urdf('outfile.urdf')
-        self.robot = p.loadURDF("outfile.urdf", self.offset, self.orientation, useFixedBase=True)
+    def import_robot(self,file_name = 'my_robot'):
+        self.name = file_name
+        self.robot = p.loadURDF(self.name+'.urdf', self.offset, self.orientation, useFixedBase=True)
         self.add_joint_sliders()
+
+        file = open(file_name+'.urdf', 'r')
+        lines = file.readlines()
+        for i, line in enumerate(lines[1:]):
+            if line.strip() == '-->':
+                break
+            dh_list = line.strip().split(',')
+            joint_info = p.getJointInfo(self.robot,self.joint_ids[i])
+            if dh_list[0] == 'revolute':
+                self.add_revolute_joint(sp.symbols(f'theta{i+1}'),float(dh_list[2]),float(dh_list[3]),float(dh_list[4]),
+                    joint_info[8],joint_info[9],joint_info[11],joint_info[10])
+            elif dh_list[0] == 'prismatic':
+                self.add_prismatic_joint(float(dh_list[1]),sp.symbols(f'd{i+1}'),float(dh_list[3]),float(dh_list[4]),
+                    joint_info[8],joint_info[9],joint_info[11],joint_info[10])
+            else:
+                self.add_fixed_joint(float(dh_list[1]),float(dh_list[2]),float(dh_list[3]),float(dh_list[4]))
         self.add_joint_frame_lines()
         self.step()
 
 
+    def interact(self):
+        subs = self.subs_joints + self.subs_additional
+        DH_params = sp.Matrix(self.links).subs(subs).evalf()
+        dh2urdf = DH2Urdf(DH_params.tolist(),self.constraints)
+        dh2urdf.save_urdf(self.name+'.urdf')
+        self.load_robot()
+       
+    def load_robot(self):
+        self.robot = p.loadURDF(self.name+'.urdf', self.offset, self.orientation, useFixedBase=True)
+        self.add_joint_sliders()
+        self.add_joint_frame_lines()
+        self.step()
+
     def add_joint_sliders(self):
         for i in range(p.getNumJoints(self.robot)):
-            joint_info = p.getJointInfo(0,i)
+            joint_info = p.getJointInfo(self.robot,i)
             if joint_info[2] == p.JOINT_PRISMATIC:
                 self.joint_ids.append(i)
-                p.resetJointState(0,i,0)
+                p.resetJointState(self.robot,i,0)
                 self.joint_sliders.append((False,p.addUserDebugParameter(f"D{i}",joint_info[8],joint_info[9],joint_info[8])))
             if joint_info[2] == p.JOINT_REVOLUTE:
                 self.joint_ids.append(i)
-                p.resetJointState(0,i,0)
+                p.resetJointState(self.robot,i,0)
                 self.joint_sliders.append((True,p.addUserDebugParameter(f"Theta{i}",np.rad2deg(joint_info[8]),np.rad2deg(joint_info[9]),0)))
     
     def add_joint_frame_lines(self):
@@ -79,18 +122,13 @@ class SerialLinkRobot:
             p.stepSimulation()
             targets = [(np.deg2rad(p.readUserDebugParameter(parameter)) if revolute else p.readUserDebugParameter(parameter)) for revolute,parameter in self.joint_sliders]
             p.setJointMotorControlArray(0,self.joint_ids,p.POSITION_CONTROL,targets)
-            time.sleep(self.timeStep)
+            time.sleep(self.time_step)
+    
+    def reset(self):
+        """ Reset the robotic arm """
+        for joint_id in self.joint_ids:
+            p.resetJointState(self.robot,joint_id,self.constraints[joint_id//2][1])
 
-    def configuration(self):
-        self.timeStep=1./60.
-        p.setTimeStep(self.timeStep)
-        p.setGravity(0,-9.8,0)
-        p.configureDebugVisualizer(lightPosition= (0,0,10))
-        p.setAdditionalSearchPath(os.getcwd())
-        p.resetDebugVisualizerCamera( cameraDistance=15, cameraYaw=15, cameraPitch=-20, cameraTargetPosition=[0,0,0])
-        #p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS,0)
-    
-    
     def add_revolute_joint(self, theta, d, a, alpha, lower = -np.pi, upper = np.pi,velocity = 2.6, effort = 10):
         """
         Add a revolute joint to the robotic arm according to the DH convention.
@@ -112,7 +150,7 @@ class SerialLinkRobot:
         self.subs_joints.append((theta, 0))
         self.constraints.append((effort, lower, upper, velocity))
 
-    def add_prismatic_joint(self, theta, d, a, alpha, lower = 0.7, upper = 4,velocity = 2.6, effort = 10):
+    def add_prismatic_joint(self, theta, d, a, alpha, lower = 0.8, upper = 4, velocity = 2.6, effort = 10):
         """
         Add a prismatic joint to the robotic arm according to the DH convention.
     
@@ -200,8 +238,9 @@ class SerialLinkRobot:
         angular_jacobian = sp.Matrix([[], [], []])
         for link in self.links:
             joint_type, theta, d, a, alpha = link
-            z_i_m1 = sp.Matrix([0, 0, 0]) if joint_type == 'prismatic' else pose[:3, 2]
-            angular_jacobian = sp.Matrix.hstack(angular_jacobian, z_i_m1)
+            z_i_m1 = sp.Matrix([0, 0, 0]) if joint_type == p.JOINT_PRISMATIC else pose[:3, 2]
+            if joint_type != p.JOINT_FIXED:
+                angular_jacobian = sp.Matrix.hstack(angular_jacobian, z_i_m1)
             pose = pose * dh_joint_to_joint(theta, d, a, alpha)
         
         angular_jacobian.simplify()
